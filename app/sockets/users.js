e252f72a92f059a9249408937798f04c4a20e7ea
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-11 23:07:42 -0300

    Add e remove

--- app/sockets/users.js
+++ app/sockets/users.js
@@ -1,5 +1,21 @@
 module.exports = function (io) {
   io.sockets.on('connection', function (client) {
     client.emit('hello', { title: 'Bem vindo', msg: 'Você está conectado' });
+
+    client.on('user_add', function (msg) {
+      client.broadcast.emit('user_add_response', {
+        title: 'Novo professor',
+        msg: 'O professor ' + msg.nome + ' foi contratado!'
+      })
+    })
+
+    client.on('user_remove', function (msg) {
+      client.broadcast.emit('user_remove_response', {
+        title: 'O professor foi removido',
+        msg: 'O professor ' + msg.nome + ' foi demitido!'
+      })
+    })
+
+
   })
 }
--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -15,5 +15,43 @@ var socket = io('http://localhost:3000');
 
 socket.on('hello', msg => {
   console.log(msg)
-  notify(msg, 'user')
+  // notify(msg, 'user')
 });
+
+socket.on('user_remove_response', msg => {
+  notify(msg, 'user_remove');
+  $('main').load('/users #content_container');
+})
+
+$('#user_remove').click(function (e) {
+  var nome = $(this).data('nome')
+  $.get($(this).attr('href'), function () {
+    socket.emit('user_remove', { nome });
+    window.location = '/users';
+  })
+
+  e.preventDefault();
+})
+
+socket.on('user_add_response', msg => {
+  notify(msg, 'user_add')
+  $('main').load('/users #content_container')
+})
+
+$('#user_add').submit(function(e) {
+  var formDataArray = $(this).serializeArray();
+  var data = {
+    nome: formDataArray[0].value
+  }
+
+  $.ajax({
+    type: "POST",
+    url: $(this).attr('action'),
+    data: $(this).serialize(),
+    success: function () {
+      socket.emit('user_add', data)
+      window.location = '/users'
+    }
+  });
+  e.preventDefault();
+})
